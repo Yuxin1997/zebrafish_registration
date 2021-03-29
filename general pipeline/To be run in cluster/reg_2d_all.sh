f027a97fa1d3a3f83607fb346b6b499faa03e0c1
Yuxin1997 <38205562+Yuxin1997@users.noreply.github.com>
2021-03-29 23:35:16 +0800

    Add files via upload

---
+++ general pipeline/To be run in cluster/reg_2d_all.sh
@@ -0,0 +1,26 @@
+folder=f1_1130/2d_120
+antsRegistrationSyNQuick.sh -d 2 -m $folder/AVG_motion_corrected_f1_120.tif -f $folder/fish1_slice61.tif -o $folder/output_f1_2d_120_
+folder=f1_1130/2d_100
+antsRegistrationSyNQuick.sh -d 2 -m $folder/AVG_motion_corrected_f1_100.tif -f $folder/fish1_slice68.tif -o $folder/output_f1_2d_100_
+folder=f1_1130/2d_80
+antsRegistrationSyNQuick.sh -d 2 -m $folder/AVG_motion_corrected_f1_80.tif -f $folder/fish1_slice73.tif -o $folder/output_f1_2d_80_
+folder=f1_1130/2d_60
+antsRegistrationSyNQuick.sh -d 2 -m $folder/AVG_motion_corrected_f1_60.tif -f $folder/fish1_slice81.tif -o $folder/output_f1_2d_60_
+
+folder=f4_1130/2d_120
+antsRegistrationSyNQuick.sh -d 2 -m $folder/AVG_motion_corrected_f4_120.tif -f $folder/fish4_slice56.tif -o $folder/output_f4_2d_120_
+folder=f4_1130/2d_100
+antsRegistrationSyNQuick.sh -d 2 -m $folder/AVG_motion_corrected_f4_100.tif -f $folder/fish4_slice62.tif -o $folder/output_f4_2d_100_
+folder=f4_1130/2d_80
+antsRegistrationSyNQuick.sh -d 2 -m $folder/AVG_motion_corrected_f4_80.tif -f $folder/fish4_slice70.tif -o $folder/output_f4_2d_80_
+folder=f4_1130/2d_60
+antsRegistrationSyNQuick.sh -d 2 -m $folder/AVG_motion_corrected_f4_60.tif -f $folder/fish4_slice77.tif -o $folder/output_f4_2d_60_
+
+folder=f6_1130/2d_120
+antsRegistrationSyNQuick.sh -d 2 -m $folder/AVG_motion_corrected_f6_120.tif -f $folder/fish6_slice61.tif -o $folder/output_f6_2d_120_
+folder=f6_1130/2d_100
+antsRegistrationSyNQuick.sh -d 2 -m $folder/AVG_motion_corrected_f6_100.tif -f $folder/fish6_slice69.tif -o $folder/output_f6_2d_100_
+folder=f6_1130/2d_80
+antsRegistrationSyNQuick.sh -d 2 -m $folder/AVG_motion_corrected_f6_80.tif -f $folder/fish6_slice76.tif -o $folder/output_f6_2d_80_
+folder=f6_1130/2d_60
+antsRegistrationSyNQuick.sh -d 2 -m $folder/AVG_motion_corrected_f6_60.tif -f $folder/fish6_slice83.tif -o $folder/output_f6_2d_60_
